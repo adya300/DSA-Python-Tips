60a2504c8932e851f561845c2f76c52ab93a07a0
Adya Bramha Samantroy <abds09nov@gmail.com>
2026-06-24 18:59:08 +0530

    Time: 11 ms (24.53%), Space: 20.4 MB (74.76%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -4,9 +4,9 @@ class Solution:
         if (len(nums)>M):
             return(M+1)
         else:
-            numss=sorted(nums)
+            nums.sort()
             count=0
-            for num in numss:
+            for num in nums:
                 if num != count:
                     return count
                 count+=1
