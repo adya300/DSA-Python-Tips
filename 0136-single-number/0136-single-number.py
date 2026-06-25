836b1c6159be9f83ed76cda558482268a7ac2bb7
Adya Bramha Samantroy <abds09nov@gmail.com>
2026-06-25 21:35:43 +0530

    Time: 0 ms (100%), Space: 21 MB (92.94%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,13 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        nums.sort()
-        d={}
+        xor = 0
         for i,v in enumerate(nums):
-            if v not in d:
-                d[v]=1
-            else:
-                d[v]+=1
-            if d[v]==2:
-                d.pop(v)
-        for k,v in d.items():
-            return k
+            xor^=v
+        return xor
