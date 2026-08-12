33624a8e771701cc6afe283398497bf4f6c38df4
Adya Bramha Samantroy <abds09nov@gmail.com>
2026-08-12 22:49:20 +0530

    Time: 0 ms (100%), Space: 20.5 MB (27.24%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -9,11 +9,10 @@ class Solution:
         poi=None
         if head==None or head.next==None:
             return head
-        while current.next!=None:
+        while current!=None:
             a=current
             next_node=current.next
             a.next=poi
             poi=a
             current=next_node
-        next_node.next=poi
-        return next_node
+        return poi
